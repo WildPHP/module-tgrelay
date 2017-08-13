a47f91ab4970ef81cf74f68cdca1bca45d3b6845
Yoshi2889 <rick.2889@gmail.com>
2017-08-13 20:42:42 +0200

    Fix php notices
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

--- src/TGRelay.php
+++ src/TGRelay.php
@@ -176,8 +176,8 @@ class TGRelay extends BaseModule
 					Logger::fromContainer($container)
 						->debug('[TG] Update received', [
 							'id' => $update->update_id,
-							'chat_id' => $update->message->chat->id,
-							'chat_name' => (!empty($update->message->chat->title) ? $update->message->chat->title : $update->message->chat->username)
+							'chat_id' => $update->message->chat->id ?? 0,
+							'chat_name' => (!empty($update->message->chat->title) ? $update->message->chat->title : ($update->message->chat->username ?? 'N/A'))
 						]);
 				}
 			}
